279c6124c8cac68204368e998b3090b6fd017d07
Paul Litvinov <tackshine@gmail.com>
2021-05-15 11:14:32 +0300

    коммит Литвинов

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace ConsoleApp2
             Console.WriteLine("Instruction 2");
             Console.WriteLine("Instruction 3");
             Console.WriteLine("Instruction another  1");
+            Console.WriteLine("Instruction another Литвинов");
         }
     }
 }
